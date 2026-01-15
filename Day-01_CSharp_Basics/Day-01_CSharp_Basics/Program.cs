dbbbb4acfd153f6e462f5bdfc30d060a8b09748c
Parmar Umang <154438572+parmar12345@users.noreply.github.com>
2026-01-16 00:32:49 +0530

    Day 1: Added first C# console program

--- Day-01_CSharp_Basics/Day-01_CSharp_Basics/Program.cs
+++ Day-01_CSharp_Basics/Day-01_CSharp_Basics/Program.cs
@@ -1,1 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is Umang.");
+        Console.WriteLine("I am starting my .NET Learning Journey.");
+    }
+}
 
